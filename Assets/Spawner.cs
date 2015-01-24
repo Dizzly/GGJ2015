dca5398d76567c6e8fa17e01c601ce8b3ce4089a
Andrea Castegnaro <andrea.castegnaro@outlook.com>
2015-01-24 11:23:42 +0000

    Added tracked position List

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -10,9 +10,14 @@ public class Spawner : MonoBehaviour {
 
 	public List<Transform> spawnPos= new List<Transform>();
 
+	//Tracking position on a sorted List
+	private List<Transform> trackedPosition = new List<Transform> ();
+	public float minimumSpacing = 3.0f;
+
 	public float minXSpawn;
 	public float maxXSpawn;
 
+	//speed given to the negative x
 	float currentSpeed_;
 	int currentScore_;
 	// Use this for initialization
@@ -21,16 +26,31 @@ public class Spawner : MonoBehaviour {
 		currentScore_ = 100;
 	}
 
+	private int CompareFunction(Transform c1, Transform c2)
+	{
+		float x1 = c1.position.x;
+		float x2 = c2.position.x;
+
+		if (x1 > x2) {
+						return 1;		
+				} else if (x1 < x2) {
+						return -1;
+				} else
+						return 0;
+	}
+
 	void SpawnKeyObj()
 	{
 		Vector3 pos = this.transform.position;
 		if(spawnPos.Count>0)
 		{
+			//Condition on general spawning
 			while(true)
 			{
 				pos=spawnPos[Random.Range(0,spawnPos.Count)].position;
 				if(pos.x>minXSpawn&&pos.x<maxXSpawn)
 				{
+					//Condition on position spacing
 					break;
 				}
 			}
@@ -42,7 +62,15 @@ public class Spawner : MonoBehaviour {
 		                                                 Quaternion.identity);
 		Mover m = g.GetComponent<Mover> ();
 		m.speed_ = currentSpeed_;
+
 		KeyObject k = g.GetComponent<KeyObject> ();
+		Rigidbody2D rg = g.GetComponent<Rigidbody2D> ();
+		rg.velocity = new Vector2 (0, Random.Range(0,8));
+
+		trackedPosition.Add(k.transform);
+		trackedPosition.Sort(CompareFunction);
+
+		//representing the color
 		int key = Random.Range (0, 4);
 		k.SetKey (key);
 		Debug.Log(key);
@@ -76,18 +104,34 @@ public class Spawner : MonoBehaviour {
 		k.Disable ();
 
 	}
-	
+
+	public void DeleteDestroyingObject(Transform tr)
+	{
+		//Assuming the order list when this function is called from KeyObject.cs we have to delete the first element
+		trackedPosition.Remove (tr);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Debug")) {
 			SpawnKeyObj();		
 		}
+		// x
 		if (Input.GetButtonDown ("Slowdown")) {
 			currentSpeed_-=0.1f;
 		}
+		// z
 		else if(Input.GetButtonDown("Speedup"))
 		{
 			currentSpeed_+=0.1f;
+			DebugPrint ();
 		}
+	}
+
+	private void DebugPrint(){
+		foreach (var tr in trackedPosition) {
+			Debug.Log(tr.position.x);
+				}
+		Debug.Log("");
 	}
 }
