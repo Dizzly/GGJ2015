51b81d5b7e1d16154bfc2f7ac84bac2ac2427617
Aldo <aldocurtis@gmail.com>
2015-01-24 08:36:45 +0000

    Added some temporary backgrounds with parralax movement

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,6 +5,8 @@ public class Parallax : MonoBehaviour {
 
 	private Vector3 newPosition;
 	public float _speed = 1.0f;
+	public float _Zoffset=1.0f;
+	public float _objWidth;
 	public bool _moveBackgound;
 	public Transform spawnTransform;
 
@@ -17,7 +19,7 @@ public class Parallax : MonoBehaviour {
 	void Update () {
 		if(_moveBackgound)
 		{
-			transform.Translate(_speed * Time.deltaTime,0f,0f);
+			transform.Translate((-_speed/_Zoffset) * Time.deltaTime,0f,0f);
 		}
 		else
 		{
@@ -26,6 +28,8 @@ public class Parallax : MonoBehaviour {
 	}
 
 	void OnBecameInvisible() {
-		transform.position = spawnTransform.position;
+		transform.position = new Vector3 (spawnTransform.position.x + _objWidth,
+		                                 spawnTransform.position.y,
+		                                 spawnTransform.position.z);
 	}
 }
